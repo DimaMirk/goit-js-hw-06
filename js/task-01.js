d2a5f22360d9c83ffbaba1dcc08fc795c2d24776
DimaMirk <dima.myruk@gmail.com>
2021-10-23 20:32:38 +0300

    hw-js-2

--- js/task-01.js
+++ js/task-01.js
@@ -12,17 +12,3 @@ const information = liItemEl.forEach((elem) => {
   const elemLength = elem.lastElementChild.children.length;
   console.log(`Elements: ${elemLength} `);
 });
-
-class CarConstroctor {
-  constructor(door, engine) {
-    this.door = door;
-    this.engine = engine;
-  }
-  showEngine = function () {
-    console.log(this.engine);
-  };
-}
-
-const audi = new CarConstroctor(4, 44);
-
-audi.showEngine();
